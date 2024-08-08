dafb73e805834637760ffb0ae5e512b11d5bdfad
Vladickush <u.vn@mail.ru>
2024-08-08 22:36:28 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -2,9 +2,7 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
-    fir = int(str_number)
-
-    if len(str_number) > 1:
+   if len(str_number) > 1:
         first = first * get_multiplied_digits(int(str_number[1:]))
     return first
 
